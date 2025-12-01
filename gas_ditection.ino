e17e85048451c66e4070490c54735b1982268293
Deepak2024714 <dv489479@gmail.com>
2025-12-01 21:42:02 +0530

    Create gas_ditection.ino
    It is simple gas ditection uning aurdiuno uno

---
+++ gas_ditection.ino
@@ -0,0 +1,22 @@
+# Gas Detector Project
+
+This is a simple *Arduino Gas Detection System* using the MQ sensor, LED, and buzzer.
+
+## Components Used
+- Arduino UNO
+- MQ-2 / MQ-6 Gas Sensor
+- LED
+- Buzzer
+- Jumper Wires
+- Breadboard
+
+## Features
+✔ Detects gas leakage  
+✔ Alerts using LED and buzzer  
+✔ Displays concentration values in serial monitor
+
+## Code
+The Arduino code for the project is inside this repository.
+
+## Author
+Deepak Verma (CSE Student)
